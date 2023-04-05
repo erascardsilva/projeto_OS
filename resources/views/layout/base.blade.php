244a03b6e4ffe87f835d9004944483b3a4090810
erascardsilva <erasmo@vmi-informatica.com.br>
2023-04-05 00:38:12 -0300

    first commit

---
+++ resources/views/layout/base.blade.php
@@ -0,0 +1,23 @@
+<!--Site criado e por Erasmo Cardoso da Silva-->
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <!--Mediaquery responsivo-->
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
+    <!--icone png do site-->
+    <link rel="icon" type="imagem/png" href="icon/vmi.png" />
+    <!--link para css e javascript-->
+    <link rel="stylesheet" href="css/stilo.css">
+    <script src="js/config.js" defer></script>
+    <title> " VMI Info|Assistência. "</title>
+</head>
+
+<body>
+    @yield('conteudo')
+
+</body>
+
+</html>
