53512a44597545c5a3318b82100e5d015c334238
erascardsilva <erasmo@vmi-informatica.com.br>
2023-04-08 12:23:08 -0300

    modificação inicio tela de login sistema

---
+++ resources/views/OS/login.blade.php
@@ -0,0 +1,18 @@
+
+@extends('layout.baselogin')
+@section('loginOS')
+    <div class="login-container">
+		<div class="login">
+			<h1>VMI O.S.</h1>
+			<form action="" method="POST">
+				<label for="username">Nome de login:</label>
+				<input type="text" id="username" name="username">
+				<label for="password">Senha:</label>
+				<input type="password" id="password" name="password">
+				<input type="submit" value="Entrar">
+
+			</form>
+            <a href="/">Voltar</a>
+		</div>
+	</div>
+@endsection
